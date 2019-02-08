1316076283d18cc0d73ce337b988e0a97381de5e
Vikash Chauhan <trickyui>
2019-02-08 11:17:11 +0530

    Form valdation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styles: ['.error{ border-color: red}', '.success{ border-color: green}']
 })
 export class LoginComponent implements OnInit {
-
+  user: any = {};
+  city = '';
   constructor() { }
-
+  onSubmit() {
+    
+   
+    if(this.city){
+      console.log(this.user);
+      console.log("city:"+this.city);
+    }
+  }
   ngOnInit() {
   }
 
 }
+
