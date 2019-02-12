f6c4fa98c72586274052712f3fb9f1a04a7a76f5
lalit <chauhan.lalit95@gmail.com>
2019-02-12 15:15:03 +0530

    adding registration form using reactive form method

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+	selector: 'app-registration',
+	templateUrl: './registration.component.html',
+	styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+	regForm : FormGroup;
+	submitted = false;
+	constructor(private formBuilder : FormBuilder) { }
 
-  constructor() { }
+	ngOnInit() {
+		this.regForm = this.formBuilder.group({
+			fname: ['', Validators.required],
+			lname: ['', Validators.required],
+			email: ['', [Validators.required, Validators.email]],
+			phone: ['', [Validators.required, Validators.pattern("[0-9]{0,10}"), Validators.minLength(10), Validators.maxLength(10)]],
+			brief: ['', Validators.required]
+		});
+	}
 
-  ngOnInit() {
-  }
+	get f(){
+		return this.regForm.controls;
+	}
+
+	onSubmit(){
+		this.submitted = true;
+
+		if (this.regForm.invalid) {
+			return;
+		}
+
+		
+		console.log('Success');
+		console.log(this.regForm);
+	}
 
 }
